535a7ad42fdf5c582aa13d85f75167288eeffcdd
Gaston Sanguinetti <gaston.sanguinetti@despegar.com>
2016-10-02 13:14:46 -0300

    Refactor in firebase view model

--- firebasedatabase/src/main/java/com/mejorana/firebasedatabase/AbstractFirebaseViewModel.java
+++ firebasedatabase/src/main/java/com/mejorana/firebasedatabase/AbstractFirebaseViewModel.java
@@ -11,17 +11,80 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.stedroids.framework.viewmodel.AbstractViewModel;
 
-/**
- * Created by gastonsanguinetti on 26/07/16.
- */
-public abstract class AbstractFirebaseViewModel<T extends ViewDataBinding> extends AbstractViewModel<T>
-    implements ValueEventListener, ChildEventListener{
-
+public abstract class AbstractFirebaseViewModel<T extends ViewDataBinding> extends AbstractViewModel<T> implements ValueEventListener, ChildEventListener {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference;
 
+    private boolean finished = false;
+    private boolean failed = false;
+
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+        finished = true;
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        finished = true;
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+        finished = true;
+    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        finished = true;
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        finished = true;
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+        finished = failed = true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if(reference == null) {
+            finished = failed = false;
+            this.reference = this.getReference(this.database);
+            if (this.attachChildEventListener()) {
+                this.reference.addChildEventListener(this);
+            }
+
+            if (this.attachValueEventListener()) {
+                this.reference.addValueEventListener(this);
+            }
+
+            if (this.attachSingleValueEventListener()) {
+                this.reference.addListenerForSingleValueEvent(this);
+            }
+        }
+    }
+
+
+
+    public void onViewModelDestroyed() {
+        if(this.attachValueEventListener() || this.attachSingleValueEventListener()) {
+            this.reference.removeEventListener((ValueEventListener) this);
+        }
+
+        if(this.attachChildEventListener()) {
+            this.reference.removeEventListener((ChildEventListener) this);
+        }
+
+        super.onViewModelDestroyed();
+    }
+
     @NonNull
-    protected abstract DatabaseReference getReference(FirebaseDatabase database);
+    protected abstract DatabaseReference getReference(FirebaseDatabase firebaseDatabase);
 
     @NonNull
     protected abstract boolean attachValueEventListener();
@@ -32,61 +95,11 @@ public abstract class AbstractFirebaseViewModel<T extends ViewDataBinding> exten
     @NonNull
     protected abstract boolean attachSingleValueEventListener();
 
-    @Override
-    public void onViewModelCreated() {
-        super.onViewModelCreated();
-        reference = getReference(database);
-        if(attachChildEventListener())
-            reference.addChildEventListener(this);
-        if(attachValueEventListener())
-            reference.addValueEventListener(this);
-        if(attachSingleValueEventListener())
-            reference.addListenerForSingleValueEvent(this);
+    public boolean isUseCaseFinished() {
+        return finished;
     }
 
-    @Override
-    public void onViewModelDestroyed() {
-        if(attachValueEventListener() || attachSingleValueEventListener()) {
-            reference.removeEventListener((ValueEventListener)this);
-        }
-        if(attachChildEventListener()) {
-            reference.removeEventListener((ChildEventListener)this);
-        }
-        super.onViewModelDestroyed();
-    }
-
-    @Override
-    public void onViewDetached(boolean finalDetachment) {
-        super.onViewDetached(finalDetachment);
-    }
-
-    @Override
-    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-        //
-    }
-
-    @Override
-    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-        //
-    }
-
-    @Override
-    public void onChildRemoved(DataSnapshot dataSnapshot) {
-        //
-    }
-
-    @Override
-    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-        //
-    }
-
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshot) {
-        //
-    }
-
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-        //
+    public boolean isUseCaseFailed() {
+        return failed;
     }
 }
